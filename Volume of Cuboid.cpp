bff3fd139e199f163fe3a42e84282f4962708db8
Tanmay Ambudkar <tanmayambudkar@gmail.com>
2025-08-29 10:54:33 +0530

    Add files via upload

---
+++ Volume of Cuboid.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class cuboid
+{
+private:
+double height=10.0,width=20.0,length=45.0;
+public:
+double volume()
+{
+double v;
+v=height*width*length;
+return v;
+}
+};
+int main()
+{
+cuboid c1;
+double vol=c1.volume();
+cout<<"Volume:"<<vol<<endl;
+}
